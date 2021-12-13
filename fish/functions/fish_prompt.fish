026e69d47361cd2516101fd70a8c62ea0cc75f94
marco <marco3jp+git@gmail.com>
2021-12-14 00:51:53 +0900

    update prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -13,8 +13,8 @@ function fish_prompt --description 'Informative prompt'
         set -l statusb_color (set_color --bold $fish_color_status)
         set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
-            $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
-            (set_color normal)
+        printf '%s%s@%s %s%s%s%s %s> ' (set_color brblue) $USER (prompt_hostname) \
+            (set_color $fish_color_cwd) $PWD (set_color normal) (__fish_vcs_prompt) \
+            $pipestatus_string
     end
 end
