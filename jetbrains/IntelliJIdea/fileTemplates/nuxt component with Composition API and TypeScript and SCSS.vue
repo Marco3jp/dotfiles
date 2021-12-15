5594767203da3cb185a2bae23f1c152b4406aa74
marco <marco3jp+git@gmail.com>
2021-12-16 01:13:37 +0900

    add file templates

---
+++ jetbrains/IntelliJIdea/fileTemplates/nuxt component with Composition API and TypeScript and SCSS.vue
@@ -0,0 +1,14 @@
+<template>
+#[[$END$]]#
+</template>
+
+<script lang="ts">
+import {defineComponent} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  name: "${COMPONENT_NAME}"
+})
+</script>
+
+<style scoped lang="scss">
+</style>
